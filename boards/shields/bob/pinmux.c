f5196ac71395e650a13b5a734ec299e650594631
Arne Wichmann <335090+arnew@users.noreply.github.com>
2025-03-30 16:14:28 +0200

    log something during gpio foo

--- boards/shields/bob/pinmux.c
+++ boards/shields/bob/pinmux.c
@@ -11,13 +11,19 @@
 #include <zephyr/sys/sys_io.h>
 #include <zephyr/devicetree.h>
 
+
+#include <zephyr/logging/log.h>
+
+LOG_MODULE_DECLARE(zmk, CONFIG_ZMK_LOG_LEVEL);
+
 static int pinmux_nice_nano_v2_init(void) {
 #if (CONFIG_BOARD_NICE_NANO_V2)
     const struct device *p0 = DEVICE_DT_GET(DT_NODELABEL(gpio0));
     gpio_pin_configure(p0, 13, GPIO_OUTPUT);
     gpio_pin_set(p0, 13, 1);
+    LOG_WRN("Enabling Power GPIO");
 #endif
     return 0;
 }
 
-SYS_INIT(pinmux_nice_nano_v2_init, APPLICATION, CONFIG_GPIO_INIT_PRIORITY);
+SYS_INIT(pinmux_nice_nano_v2_init, EARLY, 10);
