c62d803acc13924a652e24ec1cf4a20ab5afd8bf
Arne Wichmann <335090+arnew@users.noreply.github.com>
2025-04-12 12:06:01 +0200

    use regulator to enable power

--- boards/shields/bob/pinmux.c
+++ boards/shields/bob/pinmux.c
@@ -3,7 +3,7 @@
  *
  * SPDX-License-Identifier: MIT
  */
-#if 1
+#if 0
 #include <zephyr/kernel.h>
 #include <zephyr/device.h>
 #include <zephyr/init.h>
@@ -17,7 +17,7 @@
 LOG_MODULE_DECLARE(zmk, CONFIG_ZMK_LOG_LEVEL);
 
 static int pinmux_nice_nano_v2_init(void) {
-#if (CONFIG_BOARD_NICE_NANO_V2) || 1
+#if (CONFIG_BOARD_NICE_NANO_V2)
     const struct device *p0 = DEVICE_DT_GET(DT_NODELABEL(gpio0));
     gpio_pin_configure(p0, 13, GPIO_OUTPUT);
     gpio_pin_set(p0, 13, 1);
